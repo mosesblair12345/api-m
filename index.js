c3c8dc1337982342c8cb7bfa6a8ea4813d93f447
moses <mosesblair00@gmail.com>
2023-05-12 10:57:13 +0300

    airtable

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 require("dotenv").config();
-const Airtable = require("airtable-node");
+let Airtable = require("airtable");
 
-app.get("/movies", (req, res) => {
-  const airtable = new Airtable({ apiKey: process.env.AIRTABLE_ACCESS_TOKEN })
-    .base(process.env.AIRTABLE_BASE)
-    .table(process.env.AIRTABLE_TABLE);
+app.get("/", (req, res) => {
+  let base = new Airtable({ apiKey: process.env.AIRTABLE_ACCESS_TOKEN }).base(
+    process.env.AIRTABLE_BASE
+  );
+
   try {
-    airtable.list({ maxRecords: 200 }).then((resp) => {
-      const movies = resp.records.map((movie) => {
-        const { id, fields } = movie;
-        const { buttonText, images, name } = fields;
-        const { url } = images[0];
-        return {
-          id,
-          buttonText,
-          name,
-          url,
-        };
+    base(process.env.AIRTABLE_TABLE)
+      .select({
+        view: "Grid view",
+      })
+      .firstPage(function (err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+          res.send(records);
+        }
       });
-      res.send(movies);
-    });
   } catch (error) {
     res.send("There was an error with the API fetch request");
   }
