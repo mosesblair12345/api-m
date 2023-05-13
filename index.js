856272e3fb4206de8d688e10e20ec5c59d4d1b8d
moses <mosesblair00@gmail.com>
2023-05-13 14:18:49 +0300

    series

--- index.js
+++ index.js
@@ -46,6 +46,37 @@ app.get("/", (req, res) => {
   }
 });
 
+app.get("/series", (req, res) => {
+  const airtable = new Airtable({ apiKey: process.env.AIRTABLE_ACCESS_TOKEN })
+    .base(process.env.AIRTABLE_BASE)
+    .table("seriesimages");
+  try {
+    airtable
+      .list({
+        maxRecords: 200,
+        pageSize: 100,
+        cellFormat: "json",
+      })
+      .then((resp) => {
+        const series = resp.records.map((movie) => {
+          const { id } = movie;
+          const { name, buttonText } = movie.fields;
+          const { url } = movie.fields.images[0];
+          return {
+            id,
+            name,
+            buttonText,
+            url,
+          };
+        });
+
+        res.send(series);
+      });
+  } catch (error) {
+    res.send("There was an error during the API call");
+  }
+});
+
 app.listen(port, () => {
   console.log("i am listening on port 3000");
 });
