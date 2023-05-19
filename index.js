0749e0e9df7ba7497692669a36c6d1c0363ba15f
moses <mosesblair00@gmail.com>
2023-05-19 10:23:06 +0300

    adding kids route

--- index.js
+++ index.js
@@ -60,18 +60,48 @@ app.get("/series", (req, res) => {
       .then((resp) => {
         const series = resp.records.map((movie) => {
           const { id } = movie;
-          const { name, buttonText, genres } = movie.fields;
+          const { name, buttonText } = movie.fields;
           const { url } = movie.fields.images[0];
           return {
             id,
             name,
             buttonText,
             url,
-            genres,
           };
         });
 
         res.send(series);
+      });
+  } catch (error) {
+    res.send("There was an error during the API call");
+  }
+});
+
+app.get("/kids", (req, res) => {
+  const airtable = new Airtable({ apiKey: process.env.AIRTABLE_ACCESS_TOKEN })
+    .base(process.env.AIRTABLE_BASE)
+    .table("kidsimages");
+  try {
+    airtable
+      .list({
+        maxRecords: 200,
+        pageSize: 100,
+        cellFormat: "json",
+      })
+      .then((resp) => {
+        const kids = resp.records.map((kid) => {
+          const { id } = kid;
+          const { name, buttonText } = kid.fields;
+          const { url } = kid.fields.images[0];
+          return {
+            id,
+            name,
+            buttonText,
+            url,
+          };
+        });
+
+        res.send(kids);
       });
   } catch (error) {
     res.send("There was an error during the API call");
